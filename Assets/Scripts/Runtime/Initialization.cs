1d06313ffa71fffc35e67f367b499135cc091c50
Dhega Febiharsa <92192654+febiharsa@users.noreply.github.com>
2025-07-10 11:30:58 +0700

    Initial Public Source Code MUVE PCA
    Initial Public Source Code

---
+++ Assets/Scripts/Runtime/Initialization.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Monos.PUNVR.Runtime.Initializations
+{
+    public class Initialization : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
